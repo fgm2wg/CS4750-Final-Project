be47d3ba204c2529e366d56b1aa47b4bb08ee972
fgm2wg <fgm2wg@virginia.edu>
2025-12-08 01:43:37 -0500

    Add back ca.pem usage for db connection

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
         'PASSWORD':  os.getenv('DB_PASS'),
         'HOST':  os.getenv('DB_HOST'),
         'PORT':  os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'ssl': {'ca': 'ca.pem'},
+        },
     }
 }
 
